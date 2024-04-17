e0a92da279760eaca38f0fe2fb186a7078bc827c
Mlowegene <genemlowe@gmail.com>
2024-04-17 09:56:25 +0300

    create a dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}) {
+}) {<body>{children}</body>
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
